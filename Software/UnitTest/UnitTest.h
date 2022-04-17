b8ca5f959ed0b4e3d8b82bbde26a1971285dbfda
Jinson <jinsongli4x@gmail.com>
2022-04-17 13:34:59 +0800

    add some unittest

---
+++ Software/UnitTest/UnitTest.h
@@ -0,0 +1,23 @@
+#ifndef UNIT_TEST_H
+#define UNIT_TEST_H
+
+ 
+/*---------------------------- C Scope ---------------------------*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void UTestBasic(void);
+void UTestAudio(void);
+void UTestScreen(void);
+void UtestServo(void);
+void UtestHusb(void);
+ 
+#ifdef __cplusplus
+}
+
+/*---------------------------- C++ Scope ---------------------------*/
+#endif
+
+
+#endif
